9271a5cf2dbac0aba96448004caa6064e7b2434d
Mellgood <cacento@hotmail.it>
2015-02-01 21:58:39 +0100

    il calendario tenta di salvare eventi ma nel db nessuna traccia..

--- Classes/Controller/CVisitBooking.php
+++ Classes/Controller/CVisitBooking.php
@@ -1,6 +1,6 @@
 <?php
 
-class CVisitBooking {
+class CVisitBooking extends FDatabase {
     
     public function getContent() {
         $VVisitBooking=  USingleton::getInstance("VVisitBooking");
@@ -9,16 +9,19 @@ class CVisitBooking {
         return $content;
     }
     
-    public function saveElement() {
+    public function saveEvent() {
         $VVisitBooking=  USingleton::getInstance("VVisitBooking");
+        $FCalendar=  USingleton::getInstance("FCalendar");
         
         $CF=$VVisitBooking->get("CF");
         $dataInizio=$VVisitBooking->get("dataInizio");
-        $eventID=$VVisitBooking->get("EventID");
+        $eventID=$VVisitBooking->get("eventID");
         $eventObject=$VVisitBooking->get("eventObject");
-        $titolo=$VVisitBooking->get("Titolo");
+        $titolo=$VVisitBooking->get("titolo");
         
-        
+        $result=$FCalendar->saveEvent($CF, $dataInizio, $eventID, $eventObject, $titolo);
+        echo $result;
+        exit();
     }
     
     
