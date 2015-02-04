55446cf1962885ec4657c9920056d1b3e38f9b59
Mellgood <cacento@hotmail.it>
2015-02-04 04:09:58 +0100

    solo 1 visita per volta

--- Classes/Foundation/FCalendar.php
+++ Classes/Foundation/FCalendar.php
@@ -13,12 +13,26 @@
  */
 class FCalendar extends FDatabase {
     public function saveEvent($CF,$dataInizio,$eventID,$titolo, $dataFine) {
-       
-        $query1="INSERT INTO `clinica`.`calendario` (`CodiceFiscalePrenotazione`, `start`, `id`, `title`, `end`)";
-        $query2="VALUES ('$CF','$dataInizio','$eventID','$titolo', '$dataFine')";
-        $query=$query1." ".$query2;
-        $this->query($query);
-        return $this->error;
+        $error=false;
+        
+        //check if it was possible to add new event
+        $q="SELECT * FROM `calendario` WHERE `start` between '$dataInizio' and '$dataFine' ";
+        $result=$this->query($q);
+        
+        //if the affected rows are more than 0, there is another event yet
+        if($this->affected_rows){
+            $error="Purtroppo un altro utente si è prenotato nell'ora selezionata. Si prega di scegliere un orario diverso";
+        }
+        else{
+            //insert Event data to the db
+            $query1="INSERT INTO `calendario` (`CodiceFiscalePrenotazione`, `start`, `id`, `title`, `end`)";
+            $query2="VALUES ('$CF','$dataInizio','$eventID','$titolo', '$dataFine')";
+            $insertEvent=$query1." ".$query2;
+            $this->query($insertEvent);
+            if ($this->error && ! $error){$error="Si è verificato un errore, si prega di riprovare: ".$this->error;}
+        }
+     
+        return $error;
     }
     
     /**
@@ -31,7 +45,7 @@ class FCalendar extends FDatabase {
      */
     public function getAllEvents($start, $end) {
         
-        $query= "SELECT * FROM `calendario` WHERE DataInizio between $start and $end ";
+        $query= "SELECT * FROM `calendario` WHERE `start` between '$start' and '$end' ";
        
         $result=  $this->associativeArrayQuery($query);
         return $result;
